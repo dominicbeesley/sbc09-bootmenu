025fdbe675ef0a5d2c921709c4ad2826518b7b80
dominicbeesley <dominic@authorityfile.co.uk>
2023-07-21 13:22:49 +0100

    FIX: was not writing 1 byte blocks

--- src/memw.c
+++ src/memw.c
@@ -3,6 +3,7 @@
 #include "memw.h"
 #include "memmap.h"
 #include "hardware.h"
+#include "flash.h"
 
 /* These are functions for reading from the memory outside the normal 64K 
 	cpu addressing range. A window at R_WINDOW is paged in from the larger
@@ -13,18 +14,18 @@ void memw_in(unsigned char *src, unsigned long dest_addr, unsigned int len) {
 	unsigned long ptr = dest_addr;
 	unsigned long end = dest_addr + len - 1;
 
-	while (ptr < end) {
+	while (len) {
 
 		// 16K aligned blocks
 		unsigned long end2 = ((ptr & 0x3FC000) == (end & 0x3FC000)) ? end : (ptr & 0x3FC000) | 0x3FFF;
 		unsigned int n = end2 - ptr + 1;
 
-		// page the block into WINDOW
-		unsigned char ix = (ptr >> 14);
 
 		if ((ptr & 0x300000) == 0) {
-			flash_write(ptr, src, len);
+			flash_write(ptr, src, n);
 		} else {
+			// page the block into WINDOW
+			unsigned char ix = (ptr >> 14);
 			mmu_16(MMU_IX_WINDOW) = ix;		
 			memcpy((void *)(R_WINDOW + (ptr & 0x3FFF)), src, n);
 		}
@@ -32,6 +33,7 @@ void memw_in(unsigned char *src, unsigned long dest_addr, unsigned int len) {
 		if (!(end2 + 1))
 			break;
 		ptr = end2+1;
+		len -= n;
 	}
 }
 
@@ -39,7 +41,7 @@ void memw_out(unsigned char *dest, unsigned long src_addr, unsigned int len) {
 	unsigned long ptr = src_addr;
 	unsigned long end = src_addr + len - 1;
 
-	while (ptr < end) {
+	while (len) {
 
 		// 16K aligned blocks
 		unsigned long end2 = ((ptr & 0x3FC000) == (end & 0x3FC000)) ? end : (ptr & 0x3FC000) | 0x3FFF;
@@ -54,6 +56,7 @@ void memw_out(unsigned char *dest, unsigned long src_addr, unsigned int len) {
 		if (!(end2 + 1))
 			break;
 		ptr = end2+1;
+		len -= n;
 	}
 }
 
